bdac341162ca4ea3c7e136335a5da48d3c0a0335
Luiz Claro <67617278+LFClaro@users.noreply.github.com>
2022-04-15 10:13:21 -0400

    Initial Commit

---
+++ LuizClaro_N01459385_A2/LuizClaro_N01459385_A2App.swift
@@ -0,0 +1,17 @@
+//
+//  LuizClaro_N01459385_A2App.swift
+//  LuizClaro_N01459385_A2
+//
+//  Created by Luiz Claro on 2022-04-15.
+//
+
+import SwiftUI
+
+@main
+struct LuizClaro_N01459385_A2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuizClaro_N01459385_A2UITests/LuizClaro_N01459385_A2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LuizClaro_N01459385_A2UITestsLaunchTests.swift
+//  LuizClaro_N01459385_A2UITests
+//
+//  Created by Luiz Claro on 2022-04-15.
+//
+
+import XCTest
+
+class LuizClaro_N01459385_A2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
